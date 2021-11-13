88cce9a173bf4cbe90a4b0b2d1f919f593d1d594
skybro2004 <skybro2004@gmail.com>
2021-11-13 14:53:56 +0000

    webviewstring

--- www/html/app/settings/js/index.js
+++ www/html/app/settings/js/index.js
@@ -1,27 +1,26 @@
 var webViewString = JSON.parse(window.AppInventor.getWebViewString())
-var setting = JSON.parse(webViewString.settings)
+var setting = webViewString.settings
 
 
 
 var basicSetting = setting.basic
 if(basicSetting.darkmode==true){
-    document.getElementById('switch-darkmode').children[0].checked = true
+    document.getElementById('switch-basic-darkmode').children[0].checked = true
 }
 if(basicSetting.notifications==true){
-    document.getElementById('switch-notification').children[0].checked = true
+    document.getElementById('switch-basic-notifications').children[0].checked = true
 }
-
 
 
 var schedularSetting = setting.schedular
-document.getElementById("select-grade").children[schedularSetting.grade - 1].setAttribute('selected', '')
-document.getElementById("select-class").children[schedularSetting.class - 1].setAttribute('selected', '')
+document.getElementById("select-schedular-grade").children[schedularSetting.grade - 1].setAttribute('selected', '')
+document.getElementById("select-schedular-class").children[schedularSetting.class - 1].setAttribute('selected', '')
 
 const selects = ["A", "C", "D", "E", "F"]
 
 for(const select of selects){
     if(schedularSetting["select" + select]!="none"){
-        var element = document.getElementById("select-select" + select)
+        var element = document.getElementById("select-schedular-select" + select)
         for(var i=0; i<element.children.length; i++){
             if(element.children[i].getAttribute("value")==schedularSetting["select" + select]){
                 element.children[i].setAttribute('selected', '')
@@ -36,7 +35,7 @@ for(const select of selects){
 
 var mealSetting = setting.meal
 for(const allergyNo of mealSetting.allergy){
-    document.getElementById('switch-allergy-' + allergyNo).children[0].checked = true
+    document.getElementById('switch-meal-allergy-' + allergyNo).children[0].checked = true
 }
 
 
@@ -49,6 +48,12 @@ function toggled(element){
     else {
         string += "-false"
     }
+    window.AppInventor.setWebViewString(string)
+}
+
+
+function selected(element){
+    var string = "optChange-" + element.getAttribute("id") + "-" + element.value
     alert(string)
     window.AppInventor.setWebViewString(string)
 }
